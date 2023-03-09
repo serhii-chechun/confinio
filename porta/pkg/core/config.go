4e0219ad57ebd8c7f9b46408882f9d30dbedcf79
Sergiy Chechun <ss@chechun.org>
2023-03-09 11:35:36 +0000

    Update: configuration design

--- porta/pkg/core/config.go
+++ porta/pkg/core/config.go
@@ -3,7 +3,18 @@ package core
 type (
 	// Configuration defines all parameters used by the application
 	Configuration struct {
+		Core struct {
+			HTTPEngines []struct {
+				Alias            string `json:"alias"`
+				ListenAddress    string `json:"listen"`
+				ListenAddressTLS string `json:"tls_listen"`
+				CertFile         string `json:"tls_cert"`
+				KeyFile          string `json:"tls_key"`
+				ServerName       string `json:"server_name"`
+			} `json:"http_engines"`
+		} `json:"core"`
 		Servers []struct {
+			HTTPEngineAlias   string   `json:"http_engine_alias"`
 			DomainNames       []string `json:"domain_names"`
 			ServerRoot        *string  `json:"server_root"`
 			ResourceLocations []struct {
@@ -15,13 +26,6 @@ type (
 					TrafficPercentage *int    `json:"traffic_percentage"`
 				} `json:"backends"`
 			} `json:"resource_locations"`
-			HTTPEngine struct {
-				ServerName       string `json:"server_name"`
-				ListenAddress    string `json:"listen"`
-				ListenAddressTLS string `json:"tls_listen"`
-				CertFile         string `json:"tls_cert"`
-				KeyFile          string `json:"tls_key"`
-			} `json:"http_engine"`
 		} `json:"servers"`
 		Upstreams []struct {
 			Alias       string `json:"alias"`
